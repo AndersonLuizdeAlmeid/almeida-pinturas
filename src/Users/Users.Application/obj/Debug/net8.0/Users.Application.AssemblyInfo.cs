93dd19a323136e411300843421b8b3e3689509db
Anderson Luiz de Almeida <anderson-luiza@hotmail.com>
2025-04-27 16:47:27 -0300

    Altered connections strings

--- src/Users/Users.Application/obj/Debug/net8.0/Users.Application.AssemblyInfo.cs
+++ src/Users/Users.Application/obj/Debug/net8.0/Users.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Users.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1f63f7284eaae6e8b07d758a2956e3dba30f6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238109874aaf4e842b47229329d5bf8a23a66097")]
 [assembly: System.Reflection.AssemblyProductAttribute("Users.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Users.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
